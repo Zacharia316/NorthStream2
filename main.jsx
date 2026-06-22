8c41b4bf7b4b061af62b7cc2d97b8b4774ccc992
Zacharia316 <zappyblues234@gmail.com>
2026-06-22 22:57:27 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NorthStreamHome from "./NorthStreamHome.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NorthStreamHome />
+  </React.StrictMode>
+);
